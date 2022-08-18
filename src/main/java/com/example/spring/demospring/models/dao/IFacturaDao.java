a2fa2eedea1dc923c98d7cce07a840442580afc0
skynoorz <aligatorbol@gmail.com>
2022-08-18 16:44:47 -0400

    hikari implemented (not tested)

--- src/main/java/com/example/spring/demospring/models/dao/IFacturaDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring.demospring.models.dao;
-
-import com.example.spring.demospring.models.entity.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IFacturaDao extends JpaRepository<Factura, Long> {
-}
--- src/main/java/com/example/spring/demospring/models/service/CuentaService.java
+++ src/main/java/com/example/spring/demospring/models/service/CuentaService.java
@@ -1,20 +1,18 @@
 package com.example.spring.demospring.models.service;
 
-import com.example.spring.demospring.models.dao.ICuentaDao;
+import static com.example.spring.demospring.models.dao.ICuentaDao.findByContratoAndNroCuenta;
 import com.example.spring.demospring.models.entity.Cuenta;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 @Slf4j
 public class CuentaService {
 
-    @Autowired
-    ICuentaDao cuentaDao;
-
-    public Cuenta findByContratoAndNroCuenta(String contrato, String nroCuenta) {
-        return this.cuentaDao.findByContratoAndNroCuenta(contrato, nroCuenta);
+    public Cuenta findByCyC(String contrato, String nroCuenta) throws SQLException {
+        return findByContratoAndNroCuenta(contrato, nroCuenta);
     }
 
 }
